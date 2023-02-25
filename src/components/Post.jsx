5dcc6391e1e85d8de3cea38807c4a24aedd0c206
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-25 14:45:41 -0300

    Create comment section in Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -37,6 +37,16 @@ export function Post() {
           </p>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
