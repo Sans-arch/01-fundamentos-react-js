d6d7378b2cd4eb0e208f71a0bc63643f2e03b1f0
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-25 09:09:42 -0300

    Create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import Post from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import './global.css';
 
 function App() {
   return (
     <div>
-      <Post 
+      <Header />
+
+      <Post
         author="Diego Fernandes" 
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit ut omnis harum porro eos. Deserunt minus atque accusamus ipsum animi, tempora impedit, nulla nostrum odio maxime minima assumenda iusto ipsa!" 
       />
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,8 +1,8 @@
-export default function Post(props) {
+export function Post(props) {
   return (
-    <>
+    <div>
       <h1>{props.author}</h1>
       <span>{props.content}</span>
-    </>
+    </div>
   )
 }
