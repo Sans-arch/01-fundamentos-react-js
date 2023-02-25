95443cdd4ce9f6ab117cc903a3603e2a3e359c27
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-25 09:37:39 -0300

    Create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import './global.css';
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit ut omnis harum porro eos. Deserunt minus atque accusamus ipsum animi, tempora impedit, nulla nostrum odio maxime minima assumenda iusto ipsa!" 
-      />
-      <Post 
-        author="Gabriel Buzzi" 
-        content="Um novo post muito legal!" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit ut omnis harum porro eos. Deserunt minus atque accusamus ipsum animi, tempora impedit, nulla nostrum odio maxime minima assumenda iusto ipsa!"
+          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal!" />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
