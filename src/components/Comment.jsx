7c30dcf22a06f36b646dd12e2dac7f987cef9806
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-26 14:04:35 -0300

    Add delete function to trash button

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "phosphor-react";
-
 import { Avatar } from "./Avatar";
-
 import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((prevCount) => prevCount + 1);
   }
 
   return (
@@ -31,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
